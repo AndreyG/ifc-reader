2defba8bb4d6e14bee87fafaee54c94445ba38ec
Andrey Davydov <andrey.a.davydov@gmail.com>
2022-12-25 13:47:42 +0300

    very very first version of unit tests (issue #17)

---
+++ tests/core/src/main.cpp
@@ -0,0 +1,51 @@
+﻿#include <ifc/File.h>
+
+#include <gtest/gtest.h>
+
+#include <boost/iostreams/device/mapped_file.hpp>
+
+#include <filesystem>
+
+static std::filesystem::path data_dir;
+
+class FileWrapper
+    : boost::iostreams::mapped_file_source
+    , public ifc::File
+{
+    FileWrapper(std::filesystem::path const & path_to_ifc)
+        : mapped_file_source(path_to_ifc.string())
+        , File(as_bytes(std::span(data(), size())), nullptr)
+    {}
+
+public:
+    static FileWrapper create(const char* filepath)
+    {
+        const auto path_to_ifc = data_dir / filepath;
+        if (!is_regular_file(path_to_ifc))
+            throw std::logic_error("'" + path_to_ifc.string() + "' is not regular file");
+
+        return FileWrapper(path_to_ifc);
+    }
+};
+
+TEST(SimpleTest, metadata)
+{
+    const auto file = FileWrapper::create("empty.ixx.ifc");
+    const auto& header = file.header();
+    ASSERT_EQ(header.major_version, ifc::Version(0));
+    ASSERT_EQ(header.minor_version, ifc::Version(43));
+    ASSERT_EQ(header.dialect, ifc::LanguageVersion(202002));
+    ASSERT_EQ(header.arch, ifc::Architecture::X64);
+}
+
+int main(int argc, char* argv[])
+{
+    testing::InitGoogleTest(&argc, argv);
+    data_dir = argv[1];
+    if (!is_directory(data_dir))
+    {
+        std::cerr << data_dir << " is not directory\n";
+        return EXIT_FAILURE;
+    }
+    return RUN_ALL_TESTS();
+}
---
+++ tests/msvc/src/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <ifc/MSVCEnvironment.h>
+#include <ifc/File.h>
+
+#include <gtest/gtest.h>
+
+#include <filesystem>
+
+static std::string data_dir;
+
+TEST(SimpleTest, metadata)
+{
+    const auto path_to_ifc = data_dir + "/empty.ixx.ifc";
+    ASSERT_TRUE(std::filesystem::is_regular_file(path_to_ifc));
+    ifc::MSVCEnvironment env(path_to_ifc + ".d.json");
+    const auto& file = env.get_module_by_bmi_path(path_to_ifc);
+    const auto& header = file.header();
+    ASSERT_EQ(header.major_version, ifc::Version(0));
+    ASSERT_EQ(header.minor_version, ifc::Version(43));
+    ASSERT_EQ(header.dialect, ifc::LanguageVersion(202002));
+    ASSERT_EQ(header.arch, ifc::Architecture::X64);
+}
+
+int main(int argc, char* argv[])
+{
+    testing::InitGoogleTest(&argc, argv);
+    data_dir = argv[1];
+    if (!std::filesystem::is_directory(data_dir))
+    {
+        std::cerr << data_dir << " is not directory\n";
+        return EXIT_FAILURE;
+    }
+    return RUN_ALL_TESTS();
+}
