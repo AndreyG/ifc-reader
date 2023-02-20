ca45629ce803a022c6a4a78e5631c0f8d6e3549d
Andrey Davydov <andrey.davydov@jetbrains.com>
2023-02-20 12:11:28 +0100

    NFC, reduce copypaste

--- lib/reflifc/include/reflifc/Query.h
+++ lib/reflifc/include/reflifc/Query.h
@@ -8,34 +8,33 @@
 
 namespace reflifc
 {
-    using namespace std::views;
+#define FILTER_AND_TRANSFORM(BaseDeclaration, kind) \
+              std::views::filter   (&BaseDeclaration::is_ ## kind) \
+            | std::views::transform(&BaseDeclaration::as_ ## kind)
 
     inline ViewOf<ScopeDeclaration> auto get_scope_declarations(Scope scope)
     {
-        return scope.get_declarations()
-            | filter(&Declaration::is_scope)
-            | transform(&Declaration::as_scope);
+        return scope.get_declarations() | FILTER_AND_TRANSFORM(Declaration, scope);
+    }
+
+    inline ViewOf<ClassOrStruct> auto get_classes_and_structs(ViewOf<ScopeDeclaration> auto declarations)
+    {
+        return declarations | FILTER_AND_TRANSFORM(ScopeDeclaration, class_or_struct);
     }
 
     inline ViewOf<ClassOrStruct> auto get_classes_and_structs(Module module)
     {
-        return module.scope_declarations()
-            | filter(&ScopeDeclaration::is_class_or_struct)
-            | transform(&ScopeDeclaration::as_class_or_struct);
+        return get_classes_and_structs(module.scope_declarations());
     }
 
     inline ViewOf<ClassOrStruct> auto get_classes_and_structs(Scope scope)
     {
-        return get_scope_declarations(scope)
-            | filter(&ScopeDeclaration::is_class_or_struct)
-            | transform(&ScopeDeclaration::as_class_or_struct);
+        return get_classes_and_structs(get_scope_declarations(scope));
     }
 
     inline std::optional<Namespace> find_namespace_by_name(Scope scope, std::string_view name)
     {
-        auto namespaces = get_scope_declarations(scope)
-            | filter(&ScopeDeclaration::is_namespace)
-            | transform(&ScopeDeclaration::as_namespace);
+        auto namespaces = get_scope_declarations(scope) | FILTER_AND_TRANSFORM(ScopeDeclaration, namespace);
 
         const auto it = std::ranges::find_if(namespaces, [name] (Namespace ns) {
             return has_name(ns, name);
@@ -45,4 +44,6 @@ namespace reflifc
             return std::nullopt;
         return *it;
     }
+
+#undef FILTER_AND_TRANSFORM
 }
